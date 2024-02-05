1119b5a7fe116894cdafb3e16b3d323858595a59
Aidan Hawthorne <18ajh14@queensu.ca>
2024-02-05 12:27:20 -0500

    Added files

---
+++ src/mini_cpu/MDR_reg.v
@@ -0,0 +1,18 @@
+module MDR_reg (input clear, clock, MDRin, read, 
+	input [31:0]BusMuxOut, input [31:0] Mdatain, output wire [31:0]BusMuxIn);
+reg [31:0]q;
+always @ (posedge clock)
+		begin
+			if (clear) begin
+				q <= 0;
+			end
+			else if (enable) begin
+			// if read is one we want Mdatain else we want BusMuxOut
+				q <= read ? Mdatain : BusMuxOut;
+			end
+		end
+	assign BusMuxIn = q;
+endmodule
+
+
+	
