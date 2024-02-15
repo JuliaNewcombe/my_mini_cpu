60c9264675019dfa45171c14f4ddca9ee59653e6
Aidan Hawthorne <18ajh14@queensu.ca>
2024-02-15 11:50:24 -0500

    Shift functions now working

--- src/mini_cpu/rotate_right.v
+++ src/mini_cpu/rotate_right.v
@@ -1,5 +1,7 @@
-module rotate_right (input [31:0] A, input amnt, output [31:0] result);
-	
-	assign result = (A >> amnt) | (A << 32-amnt);
-	
-endmodule
+module rotate_right (input [31:0] A, B, output [31:0] result);
+	reg [31:0] q;
+	always @ (A or B) begin
+		q = (A >>  B) | (A << 32-B);
+	end
+	assign result = q;
+endmodule 
--- src/mini_cpu/shift_left.v
+++ src/mini_cpu/shift_left.v
@@ -1,5 +1,7 @@
-module shift_left (input [31:0] A, input amnt, output [31:0] result);
-	
-	assign result = A << amnt;
-	
-endmodule
+module shift_left (input [31:0] A, B, output [31:0] result);
+	reg [31:0] q;
+	always @ (A or B) begin
+		q = A << B;
+	end
+	assign result = q;
+endmodule 
