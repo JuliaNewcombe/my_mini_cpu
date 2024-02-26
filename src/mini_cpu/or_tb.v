27cb1b9b2f31803663891cd5d898545ab33176d1
Aidan Hawthorne <18ajh14@queensu.ca>
2024-02-25 19:34:17 -0500

    fixed stuff for julia

--- src/mini_cpu/or_tb.v
+++ src/mini_cpu/or_tb.v
@@ -15,8 +15,8 @@ module or_tb;
 	R0out, R1out, R2out, R3out, R4out, R5out, R6out, R7out,R8out, R9out, R10out, R11out, R12out, R13out, R14out, R15out, 
 	HIOut, LOout, Zhighout, Zlowout, PCout, MDRout, InPortout, Yout, 
 	R0in, R1in, R2in, R3in, R4in, R5in, R6in, R7in,R8in, R9in, R10in, R11in, R12in, R13in, R14in, R15in, 
-	HIin, Loin, ZHighin, Zlowin, InPC, MDRin, InPortin, Yin, 
-	BusOut, mdrData, BusMuxInR0, BusMuxInR1, BusMuxInR2, BusMuxInYOut);
+	HIin, LOin, ZHighin, Zlowin, InPC, MDRin, InPortin, Yin, 
+	BusOut, mdrData, BusMuxInR0, BusMuxInR1, BusMuxInR2, BusMuxInYOut, BusMuxInHI, BusMuxInLO);
 
 	
 initial begin
