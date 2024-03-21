1f306063c3f35cfeef7d53d62a111fef899b8352
Julia Newcombe <18jmcn@queensu.ca>
2024-03-21 16:13:04 -0400

    br tb working

--- src/mini_cpu/con_ff.v
+++ src/mini_cpu/con_ff.v
@@ -1,11 +1,12 @@
 module con_ff (output result, input [31:0] ir, input [31:0] BusMuxOut, input CONin);
 
 reg [3:0] decoder;
-reg temp;
+wire temp;
 wire equal = (BusMuxOut == 32'b0) ? 1'b1 : 1'b0;
 wire not_equal = (BusMuxOut != 32'b0) ? 1'b1 : 1'b0;
 wire positive = (BusMuxOut[31] == 1'b0) ? 1'b1 : 1'b0;
 wire negative = (BusMuxOut[31] == 1'b1) ? 1'b1 : 1'b0;
+wire eq, no, pos, neg;
 
 always @ (ir) begin
 	case (ir[20:19])
@@ -14,8 +15,13 @@ always @ (ir) begin
 			2'b10: decoder = 4'b0100;
 			2'b11: decoder = 4'b1000;
 	endcase
-	temp = ((decoder[3] & equal) | (decoder[2] & not_equal) | (decoder[1] & positive) | (decoder[0] & negative));
 end
+
+assign eq = (decoder[0] && equal);
+assign no = (decoder[1] && not_equal);
+assign pos = (decoder[2] && positive);
+assign neg = (decoder[3] &&	negative);
+assign temp = (eq || no || pos || neg);
 
 d_flip_flop	CONFF(CONin, temp, result); 
 
